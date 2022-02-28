d6daed0e4da030b96b6c4eb86a27d84f3413c2cd
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-02-28 09:05:19 -0600

    added routes directory and link in server.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
+// express requirements
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
+// set PORT and app parameters
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// point to routes
+require('./routes/routes')(app);
 
 
-
-
-
-
-
-
+// set server PORT listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
