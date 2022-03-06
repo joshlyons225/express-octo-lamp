dd439615bacad1f5531f6b1473c93e6a50faef8d
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-06 13:44:55 -0600

    updated routes.js with /api and moved dbUpdate()

--- routes/routes.js
+++ routes/routes.js
@@ -6,36 +6,41 @@ const path = require('path');
 module.exports = app => {
 
     // pull notes from database annnd convert to JSON
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile("db/db.json","utf8", (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
 
+        // GET route for notes api
+        app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
+
+        // create api POST function and push to update database
+        app.post("/api/notes", function(req, res) {
+            const newNote = req.body;
+            req.body.id = randomUUID;
+            notes.push(newNote);
+            dbUpdate();
+            return console.log("Saved note: "+newNote.title);
+        });
+
+        // show notes.html
+        app.get('/notes', function (req, res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+
+        // show index.html
+        app.get("/", function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
         // function to update database with new notes
-        function dbUpdate () {
+        function dbUpdate() {
             fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), err => {
                 if (err) throw err;
                 return true;
             });
         }
-
-        // create api GET function and convert notes to JSON
-        app.get("/notes", function(req, res) {
-            res.json(notes);
-        });
-
-        // create api POST function and push to update database
-        app.post("/notes", function(req, res) {
-            const newNote = req.body;
-            req.body.id = randomUUID;
-            notes.push(newNote);
-            res.sendFile(__dirname + notes);
-            res.json(req.body);
-        });
-
-        // create GET function for index.html
-        app.get("/", function(req, res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
-        });
     });
 }
 
